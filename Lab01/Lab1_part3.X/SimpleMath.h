e8e329cbf7318822534a00b9d5c2ed0e54de1693
jose santiago <jlsantia@ucsc.edu>
2018-07-02 13:07:17 -0700

    created SimpleMath.h

---
+++ Lab01/Lab1_part3.X/SimpleMath.h
@@ -0,0 +1,17 @@
+/*Module containing implementations of
+ *Add two numbers and Increment by one*/
+#ifndef SIMPLEMATH_H
+#define SIMPLEMATH_H
+
+/*SquarePlusOne squares x, adds one, and returns the result
+ *x is not changed
+ */
+int SquarePlusOne(int x);
+
+/*DoubleLargest finds the larger of x and y,
+ * and returns that number
+ * Neither x or y is changed
+ */
+int DoubleLargest(int y, int x);
+
+#endif
