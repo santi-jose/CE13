4affa6269789d626af7b4407d7612007e81d5590
jose santiago <jlsantia@ucsc.edu>
2018-07-02 13:55:05 -0700

    finished SimpleMath.c files

--- Lab01/Lab1_part3.X/SimpleMath.c
+++ Lab01/Lab1_part3.X/SimpleMath.c
@@ -8,6 +8,7 @@
 
 #include "SimpleMath.h"
 
+//Square plus one function: Squares value then adds one
 int SquarePlusOne(int x){
     //declare local temporary variable
     int temp;
@@ -15,10 +16,46 @@ int SquarePlusOne(int x){
     //set equal to x for math operations
     temp = x;
     
-    //square temp;
+    //square temp
     temp = temp * temp;
     
     //add one
     temp++;
+    
+    //return temp
+    return temp;
 }
 
+//DoubleLargest: doubles the largest of both values
+int DoubleLargest(int y, int x){
+    //declare local temporary variable
+    int temp;
+    
+    //if x is greater than y
+    if(x > y){
+        //set temporary equal to greater value
+        temp = x;
+        
+        //double the value
+        temp = 2 * temp;
+        
+        //return value
+        return temp;
+    }
+    //else if y is greater than x
+    else if(y > x){
+        //set temporary equal to greater value
+        temp = y;
+        
+        //double the value
+        temp = 2 * temp;
+        
+        //return value
+        return temp;
+    }
+    //if y and x are equal print message
+    else if(y == x){
+        printf("y and x are the same");
+        return;
+    }
+}
