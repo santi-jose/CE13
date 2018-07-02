dd671804224bf3bf55c4924b1a98f7a91d5fdd4e
jose santiago <jlsantia@ucsc.edu>
2018-07-02 13:14:56 -0700

    made SimpleMath.c with SquarePlusOne version 1

---
+++ Lab01/Lab1_part3.X/SimpleMath.c
@@ -0,0 +1,24 @@
+/*
+ * File:   SimpleMath.c
+ * Author: Jose Luis Santiago
+ *
+ * Created on July 2, 2018, 1:08 PM
+ */
+
+
+#include "SimpleMath.h"
+
+int SquarePlusOne(int x){
+    //declare local temporary variable
+    int temp;
+    
+    //set equal to x for math operations
+    temp = x;
+    
+    //square temp;
+    temp = temp * temp;
+    
+    //add one
+    temp++;
+}
+
