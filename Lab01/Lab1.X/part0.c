5615e9601064b0383cf083b74cd03626efb6c0bd
jose santiago <jlsantia@ucsc.edu>
2018-07-01 22:13:55 -0700

    renamed Part0 to part0

---
+++ Lab01/Lab1.X/part0.c
@@ -0,0 +1,17 @@
+/*
+ * File:   part0.c
+ * Author: Jose Luis Santiago
+ *
+ * Created on June 27, 2018, 12:13 PM
+ */
+
+
+#include "xc.h"
+#include "BOARD.h"
+
+int main(void)
+{
+    BOARD_Init();
+    printf("Hello World\n");
+    while(1);
+}
