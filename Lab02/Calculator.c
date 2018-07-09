6e854da78e70c9b5ca2bbdb7cdfb7142a7d78061
Jose Luis Santiago <jlsantia@ucsc.edu>
2018-07-08 23:17:22 -0700

    Finished all operations with proper input and output control for all operations except tangent

--- Lab02/Calculator.c
+++ Lab02/Calculator.c
@@ -15,87 +15,92 @@ double Multiply(double operand1, double operand2);
 double AbsoluteValue(double operand);
 double FahrenheitToCelsius(double operand);
 double Tangent(double operand);
-
-
+double Divide(double opearand1, double operand2);
+double Subtract(double operand1, double operand2);
+double CelsiusToFahrenheit(double operand);
+double Average(double operand1, double operand2);
 
 void CalculatorRun(void)
 {
-    printf("\n\nWelcome to Jose's calculator program! Compiled at %s %s\n",__DATE__, __TIME__);
-    while(1){
-    //Infinite prompt message for operations
-    printf("Choose one of the following operations:( '*' , '/' , '+' , '-' , 'm' , 'a' , 'c' , 'f' , 't' )\n");
-    char operation;     //variable to store user operation input
-    scanf(" %c",&operation);    //getting operation user input
-    double operand1; //operand 1 and 2 used for binary arithmetic functions
-    double operand2;
-    //checking for valid binary operations
-    if(operation == '*' || operation == '/'|| operation=='+'||operation=='-'){
-        //prompt user for operands necessary for binary arithmetic operations
-        printf("You input: %c\n",operation);    //printing operation input
-        printf("Enter operand:\n"); //prompt first operand
-        scanf(" %f",&operand1); //take first operand
-        printf("Enter second operand:\n");  //prompt second operand
-        scanf(" %f",&operand2); //take second operand
-        printf("You entered:%f %c %f\n",operand1,operation,operand2);   //print whole operation
-        
-        //perform appropriate binary arithmetic operations
-        if(operation=='*'){
-            double result = operand1 *operand2;
-            printf("Your result is:%f\n",result);
+    printf("\n\nWelcome to Jose's calculator program! Compiled at %s %s\n", __DATE__, __TIME__);
+    while (1) {
+        //Infinite prompt message for operations
+        printf("Choose one of the following operations:( '*' , '/' , '+' , '-' , 'm' , 'a' , 'c' , 'f' , 't' )\n");
+        char operation; //variable to store user operation input
+        scanf(" %c", &operation); //getting operation user input
+        double operand1; //operand 1 and 2 used for binary arithmetic functions
+        double operand2;
+        //checking for valid binary operations
+        if (operation == '*' || operation == '/' || operation == '+' || operation == '-' || operation == 'm') {
+            //prompt user for operands necessary for binary arithmetic operations
+            printf("You input: %c\n", operation); //printing operation input
+            printf("Enter operand:\n"); //prompt first operand
+            scanf(" %f", &operand1); //take first operand
+            printf("Enter second operand:\n"); //prompt second operand
+            scanf(" %f", &operand2); //take second operand
+            printf("You entered:%f %c %f\n", operand1, operation, operand2); //print whole operation
+
+            //perform appropriate binary arithmetic operations
+            if (operation == '*') {
+                double result = Multiply(operand1, operand2);
+                printf("Your result is:%f\n", result);
+            }
+            if (operation == '/') {
+                double result = Divide(operand1, operand2);
+                printf("Your result is:%f\n", result);
+            }
+            if (operation == '+') {
+                double result = Add(operand1, operand2);
+                printf("Your result is:%f\n", result);
+            }
+            if (operation == '-') {
+                double result = Subtract(operand1, operand2);
+                printf("Your result is:%f\n", result);
+            }
+            if (operation == 'm') {
+                double result = Average(operand1, operand2);
+                printf("The average is:%f\n", result);
+            }
+
+        }            //checking for valid unary operations
+        else if (operation == 'm' || operation == 'a' || operation == 'c' || operation == 'f' || operation == 't') {
+            //prompt user to enter single operand
+            printf("Enter operand:\n");
+            scanf(" %f", &operand1);
+            //print the operation they have requested
+            printf("You entered:%c %f\n", operation, operand1);
+            if (operation == 'a') {
+                double result = AbsoluteValue(operand1);
+                printf("The absolute value is:%f\n", result);
+            }
+            if (operation == 'c') {
+                double result = CelsiusToFahrenheit(operand1);
+                printf("Your result (from C->F) is:%f degrees Fahrenheit\n", result);
+            }
+            if (operation == 'f') {
+                double result = FahrenheitToCelsius(operand1);
+                printf("Your result (from F->C) is:%f degrees Celsius\n", result);
+            }
+            if (operation == 't') {
+                double result = Tangent(operand1);
+                printf("Tan= %f\n", result);
+            }
+        } else {
+            printf("error invalid operator\n");
         }
-        if(operation=='/'){
-            double result = operand1/operand2;
-            printf("Your result is:%f\n",result);
-        }
-        if(operation=='+'){
-            double result = Add(operand1,operand2);
-            printf("Your result is:%f\n",result);
-        }
-        if(operation=='-'){
-            double result = operand1 - operand2;
-            printf("Your result is:%f\n",result);
-        }
-        
-    }
-    //checking for valid unary operations
-    else if(operation=='m'||operation=='a'||operation=='c'||operation=='f'||operation=='t'){
-        //prompt user to enter single operant
-        printf("Enter operand:\n");
-        scanf(" %f",&operand1);
-        //print the operation they have requested
-        printf("You entered:%c %f\n",operation,operand1);
-        if(operation=='m'){
-           
-        }
-        if(operation=='a'){
-            double result = AbsoluteValue(operand1);
-            printf("Your result is:%f", result);
-        }
-        if(operation=='c'){
-            
-        }
-        if(operation=='f'){
-            
-        }
-        if(operation=='t'){
-            
-        }
-    }
-    else{
-        printf("error invalid operator\n");
-    }
     }
     while (1);
 }
 
-
 /********************************************************************************
  * Define the Add function here.
  ********************************************************************************/
-double Add(double operand1, double operand2){
+double Add(double operand1, double operand2)
+{
     double result = operand1 + operand2;
     return result;
 }
+
 /********************************************************************************
  * Define the Subtract function here.
  ********************************************************************************/
@@ -108,55 +113,72 @@ double Subtract(double operand1, double operand2)
 /********************************************************************************
  * Define the Multiply function here.
  ********************************************************************************/
-double Multiply(double operand1, double operand2){
+double Multiply(double operand1, double operand2)
+{
     double result = operand1 * operand2;
     return result;
 }
+
 /********************************************************************************
  * Define the Divide function here.
  ********************************************************************************/
 double Divide(double operand1, double operand2)
 {
-    double result = operand1/operand2;
-    return result;
+    if (operand2 == 0) {
+        printf("invalid denominator\n");
+    } else {
+        double result = operand1 / operand2;
+        return result;
+    }
 }
 
 /********************************************************************************
  * Define the Absolute Value function here.
  ********************************************************************************/
-double AbsoluteValue(double operand){
-    if(operand<0){
+double AbsoluteValue(double operand)
+{
+    if (operand < 0) {
         double result = operand * -1;
         return result;
-    }
-    else{
+    } else {
         return operand;
     }
-}
-/*********************************************************************************
- * Define the Fahrenheit to Celsius function here.
- ********************************************************************************/
-double CelsiusToFahrenheit(double operand)
-{
-    //  Your code here
 }
 
 /*********************************************************************************
  * Define the Celsius to Fahrenheit function here.
  ********************************************************************************/
+double CelsiusToFahrenheit(double operand)
+{
+    double result = (9.0 / 5.0) * operand + 32;
+    return result;
+}
+
+/*********************************************************************************
+ * Define the Fahrenheit to Celsius function here.
+ ********************************************************************************/
+double FahrenheitToCelsius(double operand)
+{
+    double result = (5.0 / 9.0)*(operand - 32.0);
+    return result;
+}
 
 /********************************************************************************
  * Define the Average function here.
  *******************************************************************************/
 double Average(double operand1, double operand2)
 {
-    //  Your code here
+    double result = (operand1 + operand2) / 2;
+    return result;
 }
 
 /*********************************************************************************
  * Define the Tangent function that takes input in degrees.
  ********************************************************************************/
+double Tangent(double operand1)
+{
 
+}
 
 /*********************************************************************************
  * Define the Round function here.
@@ -167,5 +189,6 @@ double Round(double operand)
 {
     //  Your code here
 }
+
 
 
