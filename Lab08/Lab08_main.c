d75002e3a64fc582b7cb9f4c84467f28a37eca0f
Jose Luis Santiago <jlsantia@ucsc.edu>
2018-08-22 20:33:33 -0700

    Replace Lab08_main.c

--- Lab08/Lab08_main.c
+++ Lab08/Lab08_main.c
@@ -10,15 +10,26 @@
 // Microchip libraries
 #include <xc.h>
 #include <plib.h>
+#include <stdio.h>
 
 // User libraries
 //------don't include BinaryTree.h or Morse.h!-----
+#include "Ascii.h"
 #include "Morse.h"
+#include "Oled.h"
+#include "OledDriver.h"
 
 //Helper functions:
 void OledAddToTopLine(MorseEvent event);
 void OledClearTopLine(MorseEvent event);
 void OledAddToBottomLine(MorseEvent event);
+
+//module level morseEvent
+static MorseEvent morseEvent;
+
+//printing arrays
+char string[50];
+char *lastChar;
 
 int main()
 {
@@ -37,25 +48,75 @@ int main()
     INTEnable(INT_T2, INT_ENABLED);
     // </editor-fold>
 
-    printf("Welcome to CRUZID's Lab8 Morse decoder!  Compiled on %s %s",__DATE__,__TIME__);
+    printf("Welcome to jlsantia's Lab8 Morse decoder!  Compiled on %s %s", __DATE__, __TIME__);
 
     //initialization code
-    
-    while (1){
-		//poll for morse events:
-        if(morseEvent){
-            //update top line of OLED
-            //decode morseEvent
-            //update bottom line of OLED
+    OledInit();
+
+
+    while (1) {
+//        OledDrawString("Hello\nWorldI'm\nWorking!\n");
+//        OledUpdate();
+
+        //poll for morse events:
+        if ((morseEvent.type == MORSE_EVENT_DOT) || (morseEvent.type == MORSE_EVENT_DASH)) {
+            OledAddToTopLine(morseEvent); //update top line of OLED
+            MorseDecode(morseEvent); //decode morseEvent
+            OledAddToBottomLine(morseEvent); //update bottom line of OLED
             //consume morseEvent
-        }	
-	};
+        }
+        if ((morseEvent.type == MORSE_EVENT_NEW_WORD) || (morseEvent.type == MORSE_EVENT_NEW_LETTER)) {
+            OledClearTopLine(morseEvent);
+        }
+    };
 }
 
 //You shouldn't need to modify this ISR!
+
 void __ISR(_TIMER_2_VECTOR, IPL4AUTO) TimerInterrupt100Hz(void)
 {
     // Clear the interrupt flag.
     IFS0CLR = 1 << 8;
-	morseEvent = MorseCheckEvents();
+
+    morseEvent = MorseCheckEvents();
 }
+
+void OledAddToTopLine(MorseEvent event)
+{
+    if (event.type == MORSE_EVENT_DOT) {
+        OledDrawString(".");
+        OledUpdate();
+    }
+    if (event.type == MORSE_EVENT_DASH) {
+        OledDrawString("-");
+        OledUpdate();
+    }
+}
+
+void OledClearTopLine(MorseEvent event)
+{
+    if ((event.type == MORSE_EVENT_NEW_WORD) || (event.type == MORSE_EVENT_NEW_LETTER)) {
+        OledDrawString("               "); //clear top line
+        OledUpdate();
+    }
+}
+
+void OledAddToBottomLine(MorseEvent event)
+{
+    if (event.type == MORSE_EVENT_CHAR_DECODED) {
+        sprintf(string, "\n%c", event.parameter);
+        OledDrawString(string);
+        OledUpdate();
+    }
+    if (event.type == MORSE_EVENT_NEW_WORD) {
+        lastChar = event.parameter;
+        sprintf(string, "\n  %s", lastChar);
+        OledDrawString(string);
+        OledUpdate();
+    }
+    if (event.type == MORSE_EVENT_ERROR) { //print ERROR
+        OledDrawString("\n#");
+        OledUpdate();
+    }
+}
+
