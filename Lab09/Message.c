f799d3d5992e41da53f2b685f7972b9e279c7184
Jose Luis Santiago <jlsantia@ucsc.edu>
2018-08-31 10:25:47 -0700

    Finished Message_Decode

--- Lab09/Message.c
+++ Lab09/Message.c
@@ -12,8 +12,24 @@
 #include <stdio.h>
 
 //macros
+#define INITIAL_INDEX 0
+#define ZERO_ASCII_VALUE 48
+#define NINE_ASCII_VALUE 57
+#define A_ASCII_VALUE 65
+#define F_ASCII_VALUE 70
 
 //variables
+static int decodePayloadIndex;
+static int decodeChecksumIndex;
+
+typedef enum{
+    WAITING, //in this state we are waiting to receive the start delimiter: '$'
+    RECORDING_PAYLOAD,
+    RECORDING_CHECKSUM
+} decodeStates;
+
+//static variable to hold our decode states
+static decodeStates decodeState = WAITING; //initial value for decodeState
 
 //helper function definitions
 uint8_t ctob(const char * string); //string converts a char from a string to a binary
@@ -24,7 +40,7 @@ uint8_t Message_CalculateChecksum(const char* payload)
 {
     uint8_t checksum = 0b00000000;
     while (1) { //while 1
-        while ((*payload == '$') || (*payload == ',') || (*payload == ' ')) { //if we get a dollar sign or comma we want to skip these chars
+        while (*payload == ',') { //if we get a dollar sign or comma we want to skip these chars
             payload++; //skip via incrementing address
         }
         uint8_t convert; //variable to store char to binary conversion
@@ -80,7 +96,7 @@ int Message_ParseMessage(const char* payload,
 
     checksumString = ChecksumStringToBinary(checksum_string);
 
-    if (payloadChecksum == checksumString) {
+    if (payloadChecksum == checksumString) { //if we receive a valid checksum and message delimiter
         if (*payload == 'C') { //challenge received
             payload++;
             if (*payload == 'H') {
@@ -89,12 +105,13 @@ int Message_ParseMessage(const char* payload,
                     message_event->type = BB_EVENT_CHA_RECEIVED;
                     payload++;
                     int j = 0; //counter to know how many chars we have in our string
-                    while (*payload != NULL) {
+                    while ((*payload != NULL) || (*payload != '*')) {
                         if (*payload == ',') {
                             payload++;
                         }
                         if (j == 0) { //only one parameter possible w/ this message
                             message_event->param0 = ctob(payload);
+                            return SUCCESS;
                         } else if (j > 0) { //invalid amount of chars return error
                             message_event->type = BB_EVENT_ERROR;
                             return STANDARD_ERROR;
@@ -102,7 +119,6 @@ int Message_ParseMessage(const char* payload,
                         payload++;
                         j++;
                     }
-
                 }
             }
         }
@@ -114,7 +130,7 @@ int Message_ParseMessage(const char* payload,
                     payload++; //just left MESSAGE string 'SHO'
                     message_event->type = BB_EVENT_SHO_RECEIVED;
                     int j = 0; //counter to know how many chars we have in our string
-                    while (*payload != NULL) {
+                    while ((*payload != NULL) || (*payload != '*')) {
                         if (*payload == ',') {
                             payload++;
                         }
@@ -125,6 +141,7 @@ int Message_ParseMessage(const char* payload,
                         if (j == 1) { //invalid amount of chars return error
                             message_event->param1 = ctob(payload);
                             message_event->param1 = AsciiHexToBin(message_event->param1);
+                            return SUCCESS;
                         } else if (j > 1) {
                             message_event->type = BB_EVENT_ERROR;
                             return STANDARD_ERROR;
@@ -144,13 +161,14 @@ int Message_ParseMessage(const char* payload,
                     payload++; //iterated through acc string, should be at ,"ACC,1"
                     message_event->type = BB_EVENT_ACC_RECEIVED;
                     int j = 0;
-                    while (*payload != NULL) {
+                    while ((*payload != '*') || (*payload != NULL)) {
                         if (*payload == ',') {
                             payload++;
                         }
                         if (j == 0) {
                             message_event->param0 = ctob(payload); //want to save the data 
                             message_event->param0 = AsciiHexToBin(message_event->param0);
+                            return SUCCESS;
                         } else if (j > 0) { //invalid amount of chars return error
                             message_event->type = BB_EVENT_ERROR;
                             return STANDARD_ERROR;
@@ -169,13 +187,14 @@ int Message_ParseMessage(const char* payload,
                     payload++;
                     message_event->type = BB_EVENT_REV_RECEIVED;
                     int j = 0; //counter to know how many chars we have in our string
-                    while (*payload != NULL) {
+                    while ((*payload != '*') || (*payload != NULL)) {
                         if (*payload == ',') {
                             payload++;
                         }
                         if (j == 0) { //only one parameter possible w/ this message
                             message_event->param0 = ctob(payload);
                             message_event->param0 = AsciiHexToBin(message_event->param0);
+                            return SUCCESS;
                         } else if (j > 0) { //invalid amount of chars return error
                             message_event->type = BB_EVENT_ERROR;
                             return STANDARD_ERROR;
@@ -188,7 +207,7 @@ int Message_ParseMessage(const char* payload,
                     payload++;
                     message_event->type = BB_EVENT_RES_RECEIVED;
                     int j = 0;
-                    while (*payload != NULL) {
+                    while ((*payload != '*') || (*payload != NULL)||(*payload != '\n')) { //when payload ends
                         if (*payload == ',') {
                             payload++;
                         }
@@ -203,6 +222,8 @@ int Message_ParseMessage(const char* payload,
                         if (j == 2) {
                             message_event->param2 = ctob(payload);
                             message_event->param2 = AsciiHexToBin(message_event->param2);
+                            
+                            return SUCCESS;
                         } else if (j > 2) {
                             message_event->type = BB_EVENT_ERROR;
                             return STANDARD_ERROR;
@@ -216,8 +237,9 @@ int Message_ParseMessage(const char* payload,
             message_event->type = BB_EVENT_ERROR;
             return STANDARD_ERROR;
         }
+    }else{
+        return STANDARD_ERROR;
     }
-    return SUCCESS;
 }
 
 uint8_t ChecksumStringToBinary(const char * checksumString)
@@ -229,8 +251,8 @@ uint8_t ChecksumStringToBinary(const char * checksumString)
         counter++; //increment counter here to tell me if i have to shift the bits
         if (*checksumString > 64) { //if receive char A-F, subtract by 55 to get decimal value of hex char
             decimal = *checksumString - 55;
-        } else if (*checksumString > 48) { //if receive char 0-9, subtract by 48 to get proper decimal value
-            decimal = *checksumString - 48;
+        } else if (*checksumString > ZERO_ASCII_VALUE) { //if receive char 0-9, subtract by 48 to get proper decimal value
+            decimal = *checksumString - ZERO_ASCII_VALUE;
         }
         if (counter < 2) { //if we are in our first iteration, shift our bits to be top 4
             checksum = decimal << 4;
@@ -253,111 +275,161 @@ uint8_t AsciiHexToBin(uint8_t ascii_hex_value) //formula that translates ascii h
         ascii_hex_value = ascii_hex_value - 48;
         return ascii_hex_value;
     }
-} 
+}
 
-int Message_Encode(char *message_string, Message message_to_encode){
+int Message_Encode(char *message_string, Message message_to_encode)
+{
     char payload[MESSAGE_MAX_PAYLOAD_LEN];
     char message[MESSAGE_MAX_LEN];
-    if(message_to_encode.type == MESSAGE_RES){
-        uint8_t checksum;   //variable to store checksum
-        
+    if (message_to_encode.type == MESSAGE_RES) {
+        uint8_t checksum; //variable to store checksum
+
         //wrap payload with PAYLOAD_RES template
-        sprintf(payload,PAYLOAD_TEMPLATE_RES,message_to_encode.param0,message_to_encode.param1,message_to_encode.param2);   //3 data fields in RES
-        checksum = Message_CalculateChecksum(payload);  //calculate checksum of payload
-        sprintf(message,MESSAGE_TEMPLATE,payload,checksum);
-        
+        sprintf(payload, PAYLOAD_TEMPLATE_RES, message_to_encode.param0, message_to_encode.param1, message_to_encode.param2); //3 data fields in RES
+        checksum = Message_CalculateChecksum(payload); //calculate checksum of payload
+        sprintf(message, MESSAGE_TEMPLATE, payload, checksum);
+
         //get size of string
         int i = 0;
-        while(message[i]!=NULL){    //while there are still contents in the array
-            *message_string = message[i];   //store them into the pointer
-            message_string++;   //increment pointer address
-            if(message[i+1] == NULL){   //return i when we have counted the last valid character in the string
+        while (message[i] != NULL) { //while there are still contents in the array
+            *message_string = message[i]; //store them into the pointer
+            message_string++; //increment pointer address
+            if (message[i + 1] == NULL) { //return i when we have counted the last valid character in the string
                 return i;
             }
-            i++;    //increment counter
+            i++; //increment counter
         }
         //return i;
     }
-    if(message_to_encode.type == MESSAGE_REV){
-        uint8_t checksum;   //variable to store checksum
-        
+    if (message_to_encode.type == MESSAGE_REV) {
+        uint8_t checksum; //variable to store checksum
+
         //wrap payload with PAYLOAD_REV template
-        sprintf(payload,PAYLOAD_TEMPLATE_REV,message_to_encode.param0);//only one param possible with REV
-        checksum = Message_CalculateChecksum(payload);  //calculate checksum of payload
-        sprintf(message,MESSAGE_TEMPLATE,payload,checksum);
-        
+        sprintf(payload, PAYLOAD_TEMPLATE_REV, message_to_encode.param0); //only one param possible with REV
+        checksum = Message_CalculateChecksum(payload); //calculate checksum of payload
+        sprintf(message, MESSAGE_TEMPLATE, payload, checksum);
+
         //get size of string
         int i = 0;
-        while(message[i]!=NULL){    //while there are still contents in the array
-            *message_string = message[i];   //store them into the pointer
-            message_string++;   //increment pointer address
-            if(message[i+1] == NULL){   //return i when we have counted the last valid character in the string
+        while (message[i] != NULL) { //while there are still contents in the array
+            *message_string = message[i]; //store them into the pointer
+            message_string++; //increment pointer address
+            if (message[i + 1] == NULL) { //return i when we have counted the last valid character in the string
                 return i;
             }
-            i++;    //increment counter
+            i++; //increment counter
         }
     }
-    if(message_to_encode.type == MESSAGE_CHA){
-        uint8_t checksum;   //variable to store checksum
-        
-        //wrap payload with PAYLOAD_RES template
-        sprintf(payload,PAYLOAD_TEMPLATE_CHA,message_to_encode.param0); //only one data field available
-        checksum = Message_CalculateChecksum(payload);  //calculate checksum of payload
-        sprintf(message,MESSAGE_TEMPLATE,payload,checksum);
-        
+    if (message_to_encode.type == MESSAGE_CHA) {
+        uint8_t checksum; //variable to store checksum
+
+        //wrap payload with PAYLOAD_CHA template
+        sprintf(payload, PAYLOAD_TEMPLATE_CHA, message_to_encode.param0); //only one data field available
+        checksum = Message_CalculateChecksum(payload); //calculate checksum of payload
+        sprintf(message, MESSAGE_TEMPLATE, payload, checksum);
+
         //get size of string
         int i = 0;
-        while(message[i]!=NULL){    //while there are still contents in the array
-            *message_string = message[i];   //store them into the pointer
-            message_string++;   //increment pointer address
-            if(message[i+1] == NULL){   //return i when we have counted the last valid character in the string
+        while (message[i] != NULL) { //while there are still contents in the array
+            *message_string = message[i]; //store them into the pointer
+            message_string++; //increment pointer address
+            if (message[i + 1] == NULL) { //return i when we have counted the last valid character in the string
                 return i;
             }
-            i++;    //increment counter
+            i++; //increment counter
         }
     }
-    if(message_to_encode.type == MESSAGE_ACC){
-        uint8_t checksum;   //variable to store checksum
-        
-        //wrap payload with PAYLOAD_RES template
-        sprintf(payload,PAYLOAD_TEMPLATE_ACC,message_to_encode.param0); //only one data field possible
-        checksum = Message_CalculateChecksum(payload);  //calculate checksum of payload
-        sprintf(message,MESSAGE_TEMPLATE,payload,checksum);
-        
+    if (message_to_encode.type == MESSAGE_ACC) {
+        uint8_t checksum; //variable to store checksum
+
+        //wrap payload with PAYLOAD_ACC template
+        sprintf(payload, PAYLOAD_TEMPLATE_ACC, message_to_encode.param0); //only one data field possible
+        checksum = Message_CalculateChecksum(payload); //calculate checksum of payload
+        sprintf(message, MESSAGE_TEMPLATE, payload, checksum);
+
         //get size of string
         int i = 0;
-        while(message[i]!=NULL){    //while there are still contents in the array
-            *message_string = message[i];   //store them into the pointer
-            message_string++;   //increment pointer address
-            if(message[i+1] == NULL){   //return i when we have counted the last valid character in the string
+        while (message[i] != NULL) { //while there are still contents in the array
+            *message_string = message[i]; //store them into the pointer
+            message_string++; //increment pointer address
+            if (message[i + 1] == NULL) { //return i when we have counted the last valid character in the string
                 return i;
             }
-            i++;    //increment counter
+            i++; //increment counter
         }
     }
-    if(message_to_encode.type == MESSAGE_SHO){
-        uint8_t checksum;   //variable to store checksum
-        
-        //wrap payload with PAYLOAD_RES template
-        sprintf(payload,PAYLOAD_TEMPLATE_SHO,message_to_encode.param0,message_to_encode.param1);//2 possible data fields
-        checksum = Message_CalculateChecksum(payload);  //calculate checksum of payload
-        sprintf(message,MESSAGE_TEMPLATE,payload,checksum);
-        
+    if (message_to_encode.type == MESSAGE_SHO) {
+        uint8_t checksum; //variable to store checksum
+
+        //wrap payload with PAYLOAD_SHO template
+        sprintf(payload, PAYLOAD_TEMPLATE_SHO, message_to_encode.param0, message_to_encode.param1); //2 possible data fields
+        checksum = Message_CalculateChecksum(payload); //calculate checksum of payload
+        sprintf(message, MESSAGE_TEMPLATE, payload, checksum);
+
         //get size of string
         int i = 0;
-        while(message[i]!=NULL){    //while there are still contents in the array
-            *message_string = message[i];   //store them into the pointer
-            message_string++;   //increment pointer address
-            if(message[i+1] == NULL){   //return i when we have counted the last valid character in the string
+        while (message[i] != NULL) { //while there are still contents in the array
+            *message_string = message[i]; //store them into the pointer
+            message_string++; //increment pointer address
+            if (message[i + 1] == NULL) { //return i when we have counted the last valid character in the string
                 return i;
             }
-            i++;    //increment counter
+            i++; //increment counter
         }
-    }else if(message_to_encode.type == MESSAGE_NONE){
+    } else if (message_to_encode.type == MESSAGE_NONE) {
         return 0;
     }
 }
 
-int Message_Decode(unsigned char char_in, BB_Event * decoded_message_event){
-    
+int Message_Decode(unsigned char char_in, BB_Event * decoded_message_event)
+{
+    switch (decodeState) {
+        static char payload[MESSAGE_MAX_PAYLOAD_LEN];  //array to store payload
+        static char checksum[3];   //checksum array to store checksum string
+        
+    case WAITING:
+        if (char_in == '$') { //start delimiter received
+            decodePayloadIndex = INITIAL_INDEX; //set indecex for arrays
+            decodeState = RECORDING_PAYLOAD;
+            return SUCCESS;
+        }else{
+            decodeState = WAITING;
+        }
+        break;
+    case RECORDING_PAYLOAD:
+        
+        if (char_in == '*') {   //if we receive our payload delimiter
+            decodeChecksumIndex = INITIAL_INDEX;
+            decodeState = RECORDING_CHECKSUM;   //go to chceksum recording
+            return SUCCESS;
+        } else {
+            payload[decodePayloadIndex] = char_in; //store valid payload char
+            decodePayloadIndex++;   //increment payload array address
+            decodeState = RECORDING_PAYLOAD;    //return to state RECORDING_PAYLOAD
+            return SUCCESS;
+        }
+
+
+        break;
+    case RECORDING_CHECKSUM:
+        if(char_in == '\n'){    //if we receive our null delimiter
+            int payloadLength;
+            payloadLength = Message_ParseMessage(payload,checksum,decoded_message_event);
+            
+            if(payloadLength){  //if the payload checksum and the checksum string match, and theMessage was parsed successfully
+                decodeState = WAITING;
+                return SUCCESS;
+            }else{
+                return STANDARD_ERROR;
+                decodeState = WAITING;
+            }
+            return SUCCESS;
+        }else if(((char_in >= ZERO_ASCII_VALUE)&&(char_in<=NINE_ASCII_VALUE))||((char_in>=A_ASCII_VALUE)&&(char_in<=F_ASCII_VALUE))){   //if we receive a valid hex value
+            checksum[decodeChecksumIndex] = char_in;    //store checksum value from input
+            decodeChecksumIndex++; //increment checksum string address
+            decodeState = RECORDING_CHECKSUM;
+            return SUCCESS;
+        }
+        break;
+    }
 }
